6bca1b130e68c114ba704da21d3f0dc3cefc42fd
Ямангулов Андрей <yamangulov@satel.org>
2022-11-07 20:04:09 +0300

    Initial commit

---
+++ src/main/java/org/satel/primefaces/PrimefacesApplication.java
@@ -0,0 +1,13 @@
+package org.satel.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimefacesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/satel/primefaces/PrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.satel.primefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimefacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
